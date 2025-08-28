5baa1dc7ade2b3c708d99577d37db94f3629e9ae
Maheshkampasati <127406159+Maheshkampasati@users.noreply.github.com>
2025-08-27 22:57:52 -0500

    Delete backend/backend/models directory

--- backend/backend/models/User.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  role: { type: String, enum: ['manager', 'staff'], default: 'staff' }
-});
-module.exports = mongoose.model('User', UserSchema);
